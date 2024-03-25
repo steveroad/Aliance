dae88ee956f7aeccefa514b3d06653eefadbabaa
steveroad <zhdankinaleksey@gmail.com>
2024-03-25 15:40:05 +0700

    fix: svg-icon css and @media

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 const navbar = document.querySelector(".navbar");
-const headerlogo = document.querySelector(".header-logo");
+const navbarlogo = document.querySelector(".navbar-logo");
 const logo = document.querySelector(".logo-svg use");
 const navbarbutton = document.querySelector(".navbar-button");
 window.addEventListener("scroll", () => {
   if (this.scrollY > 1) {
     navbar.classList.add("navbar-light");
-    headerlogo.classList.add("header-logo-light");
+    navbarlogo.classList.add("navbar-logo-light");
     logo.href.baseVal = "img/sprite.svg#logo_dark";
     navbarbutton.classList.add("navbar-button-light");
   } else {
     navbar.classList.remove("navbar-light");
-    headerlogo.classList.remove("header-logo-light");
+    navbarlogo.classList.remove("navbar-logo-light");
     logo.href.baseVal = "img/sprite.svg#logo_light";
     navbarbutton.classList.remove("navbar-button-light");
   }
