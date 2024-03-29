f01de9799f890f8f3c4a7a085362afed1fe5106a
steveroad <zhdankinaleksey@gmail.com>
2024-03-29 20:33:57 +0700

    add: js openMenu and closeMenu

--- js/main.js
+++ js/main.js
@@ -5,21 +5,52 @@ const navbarbutton = document.querySelector(".navbar-button");
 
 const mMenuToogle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
+
+const lightModeOn = (event) => {
+  navbar.classList.add("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo_dark";
+};
+
+const lightModeOff = (event) => {
+  navbar.classList.remove("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo_light";
+};
+
+const openMenu = (event) => {
+  //Функция открывания меню
+  menu.classList.add("is-open"); //Вешает класс is-open
+  mMenuToogle.classList.add("is-close");
+  document.body.style.overflow = "hidden"; //Запрещаем прокрутку сайта под меню
+  lightModeOn();
+};
+
+const closeMenu = (event) => {
+  //Функция закрывания меню
+  menu.classList.remove("is-open"); //Убирает класс is-open
+  mMenuToogle.classList.remove("is-close");
+  document.body.style.overflow = "visible"; //Возвращает прокрутку сайта под меню
+  lightModeOff();
+};
+/////////
 window.addEventListener("scroll", () => {
   if (this.scrollY > 1) {
-    navbar.classList.add("navbar-light");
+    // this.scrollY > 1 ? lightModeOn() : lightModeOff();  // Аналог
+    lightModeOn();
     navbarlogo.classList.add("navbar-logo-light");
-    logo.href.baseVal = "img/sprite.svg#logo_dark";
     navbarbutton.classList.add("navbar-button-light");
   } else {
-    navbar.classList.remove("navbar-light");
+    lightModeOff();
     navbarlogo.classList.remove("navbar-logo-light");
-    logo.href.baseVal = "img/sprite.svg#logo_light";
     navbarbutton.classList.remove("navbar-button-light");
   }
 });
 
 mMenuToogle.addEventListener("click", (event) => {
   event.preventDefault();
-  menu.classList.toggle("is-open");
+  //menu.classList.toggle("is-open");
+  if (menu.classList.contains("is-open")) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
 });
