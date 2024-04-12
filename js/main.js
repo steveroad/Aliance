f125223f3e64c110df9d3e9c80e3b07fc2f8e4d2
steveroad <zhdankinaleksey@gmail.com>
2024-04-12 19:34:44 +0700

    fix: modal js

--- js/main.js
+++ js/main.js
@@ -138,7 +138,7 @@ const swiperBlog = new Swiper(".blog-swiper", {
   },
 });
 /////////
-const modal = document.querySelector(".modal");
+/*const modal = document.querySelector(".modal");
 const modalToggle = document.querySelectorAll("[data-toggle=modal]");
 const modalClose = document.querySelector(".modal-close");
 modalToggle.forEach((element) => {
@@ -150,4 +150,23 @@ modalToggle.forEach((element) => {
 modalClose.addEventListener("click", (event) => {
   event.preventDefault();
   modal.classList.remove("is-open");
+});*/
+const modal = document.querySelector(".modal");
+const modalDialog = document.querySelector(".modal-dialog");
+
+document.addEventListener("click", (event) => {
+  if (
+    event.target.dataset.toggle == "modal" ||
+    event.target.parentNode.dataset.toggle == "modal" ||
+    (!event.composedPath().includes(modalDialog) &&
+      modal.classList.contains("is-open"))
+  ) {
+    event.preventDefault();
+    modal.classList.toggle("is-open");
+  }
+});
+document.addEventListener("keyup", (event) => {
+  if (event.key == "Escape" && modal.classList.contains("is-open")) {
+    modal.classList.toggle("is-open");
+  }
 });
