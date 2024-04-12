732eec4046156f70c27acd8be53f728570b46c86
steveroad <zhdankinaleksey@gmail.com>
2024-04-12 17:56:28 +0700

    fix: modal

--- js/main.js
+++ js/main.js
@@ -137,3 +137,17 @@ const swiperBlog = new Swiper(".blog-swiper", {
     },
   },
 });
+/////////
+const modal = document.querySelector(".modal");
+const modalToggle = document.querySelectorAll("[data-toggle=modal]");
+const modalClose = document.querySelector(".modal-close");
+modalToggle.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    event.preventDefault();
+    modal.classList.add("is-open");
+  });
+});
+modalClose.addEventListener("click", (event) => {
+  event.preventDefault();
+  modal.classList.remove("is-open");
+});
